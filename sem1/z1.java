b2a7410a8e4a74861d7e021d5aaec2c13fb76faa
Anna.K <kkk.aanya1996@mail.ru>
2022-12-13 13:48:06 +0300

    решила задачу 1 про генерацию целого числа в диапазоне от 0 до 2000

--- sem1/z1.java
+++ sem1/z1.java
@@ -1,18 +1,12 @@
 /* Практическое задание №1.
-Задача 1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i.
+Задача 1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i. */
 
-Пункты реализовать в методе main
-*Пункты реализовать в разных методах
-int i = new Random().nextInt(k); //это кидалка случайных чисел!) */
 package sem1;
-
 import java.util.Random;
-
 public class z1 {
     public static void main(String[] args) {
-        Random rand = new.Random().nextInt(2000);
-        int i = new Random(2000);
-        System.out.println (i); 
+        int i = new Random().nextInt(2000);
+        System.out.println ("Случайное целое число в диапазоне от 0 до 2000 равняется: "+i);
     }
 }
 
