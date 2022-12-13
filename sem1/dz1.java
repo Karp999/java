a24db75d90155e42bef41d557061eae9d61522bc
Anna.K <kkk.aanya1996@mail.ru>
2022-12-13 20:33:36 +0300

    решена задача 3 в методе firstArray(i, n)

--- sem1/dz1.java
+++ sem1/dz1.java
@@ -10,30 +10,47 @@
 int i = new Random().nextInt(k); //это кидалка случайных чисел!)*/
 
 package sem1;
+import java.util.Arrays;
 import java.util.Random;
 public class dz1 {
     // первоначально у меня пойдёт метод main
     public static void main(String[] args) {
         int i = randNum();
-        System.out.println("Случайное целое число в диапазоне от 0 до 2000 равняется: " + i);
+        System.out.println("Случайное целое число в диапазоне от 0 до 2000 равняется i = " + i);
         int n = mostSignificantBit(i);
-        System.out.println("Номер старшего значащего бита выпавшего числа i: "+ n);
-
-//        firstArray();
+        System.out.println("Номер старшего значащего бита выпавшего числа i равен n = " + n);
+        firstArray(i, n);
 //        secondArray();
     }
-    static int randNum(){
+
+    static int randNum() {
         return new Random().nextInt(2001);
-    }
-    static int mostSignificantBit(int i){
+    } // решение задачи 1
+
+    static int mostSignificantBit(int i) {
         return Integer.toBinaryString(i).length();
-    }
-//    static void firstArray() {
-//
-//    }
+    } // решение задачи 2
+
+    static void firstArray(int i, int n) {
+        int max = Short.MAX_VALUE;
+        int[] m1 = new int[0];
+        while (i <= max){
+            if (i % n == 0){
+                int[] temp = new int[m1.length + 1];
+                for (int g = 0; g < m1.length; g++){
+                    temp[g] = m1[g];
+                }
+                m1 = temp;
+                m1[m1.length - 1] = i;
+            }
+            i++;
+        }
+        System.out.println("Массив кратных n чисел в диапазоне от i до константы Short.MAX_VALUE = 32767 равен: \n"+ Arrays.toString(m1));
+    } // решение задачи 3
+
 //
 //    static void secondArray() {
-
+//        System.out.println("Массив кратных n чисел в диапазоне от i до константы Short.MIN_VALUE );
 //    }
 //
 
