a525637102bed4560ebbcb2ce427e57eeff44b59
Anna.K <kkk.aanya1996@mail.ru>
2022-12-19 14:59:32 +0300

    Решила задания 3, 4, 5 - дз3

--- sem3/dz3.java
+++ sem3/dz3.java
@@ -8,7 +8,8 @@ public class dz3 {
         ArrayList<String> flowers = new ArrayList<String>();
         flowers.add("rose");
         flowers.add("lilac");
-        flowers.add("iris");;
+        flowers.add("iris");
+        ;
         flowers.add("violet");
         flowers.add("carnation");
         flowers.add("forget-me-not");
@@ -19,22 +20,29 @@ public class dz3 {
 //2. Итерация всех элементов списка цветов и добавления к каждому символа '!'.
         System.out.println("\n2.Итерация: ");
         ListIterator<String> iteratorF = flowers.listIterator();
-        while (iteratorF.hasNext()){
+        while (iteratorF.hasNext()) {
             iteratorF.set(iteratorF.next().concat("!"));
         }
         System.out.println(flowers);
 
 //3. Вставить элемент в список в первой позиции.
+        System.out.println("\n3.Добавление элемента: ");
+        flowers.add(0, "aster");
+        System.out.println(flowers);
 
+//4. Извлечь элемент (по указанному индексу) из заданного списка.
+        System.out.println("\n4.Извлечение элемента: ");
+        System.out.println(flowers.get(0));
 
+//5. Обновить определенный элемент списка по заданному индексу.
+        System.out.println("\n5.Обновление элемента: ");
+        flowers.set(0,"aster!");
+        System.out.println(flowers.get(0));
 
 
     }
 
 
-
-//4. Извлечь элемент (по указанному индексу) из заданного списка.
-//5. Обновить определенный элемент списка по заданному индексу.
 //6. Удалить третий элемент из списка.
 //7. Поиска элемента в списке по строке.
 //8. Создать новый список и добавить в него несколько элементов первого списка.
