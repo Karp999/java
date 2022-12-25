7cdee77e0985ca58f081b04acec61836881e4612
Anna.K <kkk.aanya1996@mail.ru>
2022-12-25 21:47:15 +0300

    5 пункт дз5

--- sem5/dz5.java
+++ sem5/dz5.java
@@ -1,26 +1,23 @@
 package sem5;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class dz5 {
-
         public static void main(String[] args) {
 //1. Создать словарь HashMap. Обобщение <Integer, String>.
-                HashMap <Integer, String> writers = new HashMap<Integer, String>();
+                HashMap<Integer, String> writers = new HashMap<Integer, String>();
 
 //2. Заполнить пятью ключами (индекс, ФИО+Возраст+Пол "Иванов Иван Иванович 28 м"), добавить ключ, если не было!)
-                writers.put(0,"Тургенев Иван Сергеевич 64 м");
-                writers.put(1,"Булгаков Михаил Афанасьевич 48 м");
-                writers.put(2,"Пастернак Борис Леонидович 70 м");
-                writers.put(3,"Гоголь Николай Васильевич 42 м");
-                writers.put(4,"Островский Александр Николаевич 63 м");
-                writers.put(5,"Достоевский Фёдор Михайлович 59 м");
-                writers.put(6,"Пушкин Александр Сергеевич 37 м");
-                writers.put(7,"Чехов Антон Павлович 44 м");
+                writers.put(0, "Тургенев Иван Сергеевич 64 м");
+                writers.put(1, "Булгаков Михаил Афанасьевич 48 м");
+                writers.put(2, "Пастернак Борис Леонидович 70 м");
+                writers.put(3, "Гоголь Николай Васильевич 42 м");
+                writers.put(4, "Островский Александр Николаевич 63 м");
+                writers.put(5, "Достоевский Фёдор Михайлович 59 м");
+                writers.put(6, "Пушкин Александр Сергеевич 37 м");
+                writers.put(7, "Чехов Антон Павлович 44 м");
 
-                Set <Map.Entry<Integer,String>> allWriters = writers.entrySet();
+                Set<Map.Entry<Integer, String>> allWriters = writers.entrySet();
                 System.out.println("\nСписок писателей (ключ+значение): ");
                 System.out.println(allWriters);
 
@@ -48,8 +45,65 @@ public class dz5 {
                 }
 
 //5. *Сортировать значения по возрасту и вывести отсортированную коллекцию как в четвёртом пункте.
+                ArrayList<String> listSurname = new ArrayList<>();
+                ArrayList<String> listName = new ArrayList<>();
+                ArrayList<String> listPatronymic = new ArrayList<>();
+                ArrayList<Integer> listAge = new ArrayList<>();
+                ArrayList<String> listGender = new ArrayList<>();
+                List <Integer> linkedlist = new LinkedList<>();
+                for (Integer keys : writers.keySet()) {
+                        String[] valuesSeparately = writers.get(keys).split(" ");
+                        listSurname.add(valuesSeparately[0]);
+                        listName.add(valuesSeparately[1]);
+                        listPatronymic.add(valuesSeparately[2]);
+                        listAge.add(Integer.parseInt(valuesSeparately[3]));
+                        listGender.add(valuesSeparately[4]);
+                        linkedlist.add(listAge.size() - 1);
+                }
+                System.out.println("\nСортировка писателей по возрасту: ");
+                int positionLink = linkedlist.size() - 1;
+                while (positionLink > -1) {
+                        int maxAge = listAge.get(linkedlist.get(positionLink));
+                        int index = positionLink;
+                        for (int i = 0; i < positionLink; i++) {
+                                if (maxAge < listAge.get(linkedlist.get(i))) {
+                                        maxAge = listAge.get(linkedlist.get(i));
+                                        index = i;
+                                }
+                        }
+                        int tempVar = linkedlist.get(positionLink);
+                        linkedlist.set(positionLink, linkedlist.get(index));
+                        linkedlist.set(index, tempVar);
+                        positionLink--;
+                }
+
+                linkedlist.forEach(i -> System.out.println(" ФИО: "+ listSurname.get(i) + " " + listName.get(i) + " " + listPatronymic.get(i)+ " , пол: " + listGender.get(i) + " , возраст: " + listAge.get(i)));
+                HashMap<Integer, String> writersNewSorted = new HashMap<Integer, String>();
+
+                for (int i = 0; i < writersNewSorted.size(); i++) {
+                        writersNewSorted.put(0, String.valueOf(listSurname.get(i)));
+                        writersNewSorted.put(1, String.valueOf(listName.get(i)));
+                        writersNewSorted.put(2, String.valueOf(listPatronymic.get(i)));
+                        writersNewSorted.put(3, String.valueOf(listGender.get(i)));
+                        writersNewSorted.put(4, String.valueOf(listAge.get(i)));
+                }
 
 
+                Set<Map.Entry<Integer, String>> writersNewSorted2 = writersNewSorted.entrySet();
+                System.out.println("\nСписок писателей (ключ+значение): ");
+                System.out.println(writersNewSorted2);
 
+                //хотела сортировать в linkedlist, а потом вернуть в таком виде в HashMap и вывести кодом из 4 задачи, но пока не получилось.
+
+
+//                for (Integer keys : keysWriters) {
+//                        String temp = writers.get(keys).split(" ")[0];
+//                        String initialsOfWriters = temp.toUpperCase().charAt(0) + temp.toLowerCase().substring(1, temp.length());
+//                        temp = writers.get(keys).split(" ")[1].toUpperCase().charAt(0) + ".";
+//                        initialsOfWriters = initialsOfWriters + " " + temp;
+//                        temp = writers.get(keys).split(" ")[2].toUpperCase().charAt(0) + ".";
+//                        initialsOfWriters = initialsOfWriters + temp;
+//                        System.out.println(initialsOfWriters);
+//                }
         }
 }
