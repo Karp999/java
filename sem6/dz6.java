3f648c094ba6cd6bcaf47ae7166d67450b5d66df
Anna.K <kkk.aanya1996@mail.ru>
2022-12-26 13:42:55 +0300

    1,2,3,4 пункты дз6

--- sem6/dz6.java
+++ sem6/dz6.java
@@ -1,9 +1,41 @@
 package sem6;
-
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeSet;
 public class dz6 {
 
     public static void main(String[] args) {
+//1. Объявить и инициализировать экземпляр класса HashSet. Обобщение String.
+        HashSet<String> NewYear = new HashSet<String>();
+//2. Добавить пять элементов в множество.
+        NewYear.add("Гирлянда");
+        NewYear.add("Игрушка");
+        NewYear.add("Ёлка");
+        NewYear.add("Мандарин");
+        NewYear.add("Оливье");
+        System.out.println("\nМножество элементов NewYear: ");
+        System.out.println(NewYear);
+//3. Отсортировать значения используя итератор. + //4. Отсортированный результат сохранить в LinkedHashSet.
+        System.out.println("\nСортировка элементов NewYear: ");
+        ArrayList<String> arrayNY = new ArrayList();
+        LinkedHashSet<String> sortElementsOfNY = new LinkedHashSet<>();
 
+        for (String elementsOfNY : NewYear) {
+            arrayNY.add(elementsOfNY);
+        }
+
+        Collections.sort(arrayNY);
+        for (String string : arrayNY) {
+            sortElementsOfNY.add(string);
+        }
+
+        System.out.println(sortElementsOfNY);
+
+//5. Создать TreeSet с компаратором.
+//6. Скопировать содержимое первого множества (HashSet) в TreeSet.
+//7. *Скорректировать компаратор так, чтобы поля хранились в обратном порядке.
 
     }
 }
