32964cec09a81fe7560d7c2a84a0b903ffb6c018
Anna.K <kkk.aanya1996@mail.ru>
2022-12-26 13:57:15 +0300

    5,6 пункты дз6

--- sem6/dz6.java
+++ sem6/dz6.java
@@ -4,25 +4,27 @@ import java.util.LinkedHashSet;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.TreeSet;
+import java.util.Comparator;
 public class dz6 {
 
     public static void main(String[] args) {
 //1. Объявить и инициализировать экземпляр класса HashSet. Обобщение String.
-        HashSet<String> NewYear = new HashSet<String>();
+        HashSet<String> NewYearHS = new HashSet<String>();
 //2. Добавить пять элементов в множество.
-        NewYear.add("Гирлянда");
-        NewYear.add("Игрушка");
-        NewYear.add("Ёлка");
-        NewYear.add("Мандарин");
-        NewYear.add("Оливье");
-        System.out.println("\nМножество элементов NewYear: ");
-        System.out.println(NewYear);
-//3. Отсортировать значения используя итератор. + //4. Отсортированный результат сохранить в LinkedHashSet.
-        System.out.println("\nСортировка элементов NewYear: ");
+        NewYearHS.add("Гирлянда");
+        NewYearHS.add("Игрушка");
+        NewYearHS.add("Ёлка");
+        NewYearHS.add("Мандарин");
+        NewYearHS.add("Оливье");
+        System.out.println("\nМножество элементов NewYearHS: " + NewYearHS);
+
+//3. Отсортировать значения используя итератор. +
+    // 4. Отсортированный результат сохранить в LinkedHashSet.
+
         ArrayList<String> arrayNY = new ArrayList();
         LinkedHashSet<String> sortElementsOfNY = new LinkedHashSet<>();
 
-        for (String elementsOfNY : NewYear) {
+        for (String elementsOfNY : NewYearHS) {
             arrayNY.add(elementsOfNY);
         }
 
@@ -31,11 +33,24 @@ public class dz6 {
             sortElementsOfNY.add(string);
         }
 
-        System.out.println(sortElementsOfNY);
+        System.out.println("\nСортировка элементов NewYearHS: " + sortElementsOfNY);
 
-//5. Создать TreeSet с компаратором.
-//6. Скопировать содержимое первого множества (HashSet) в TreeSet.
-//7. *Скорректировать компаратор так, чтобы поля хранились в обратном порядке.
+//5. Создать TreeSet с компаратором. +
+    // 6. Скопировать содержимое первого множества (HashSet) в TreeSet. +
+        //7. *Скорректировать компаратор так, чтобы поля хранились в обратном порядке.
+        TreeSet<String> NewYearTS = new TreeSet<>(new Comparator<String>(){
+            @Override
+            public int compare(String el1, String el2) {
+                if (el1.equals(el2)) {
+                    return 0;
+                } if (el1.compareTo(el2) < 0) {
+                    return 1;
+                } else return -1;
+            }
+        });
+
+        NewYearTS.addAll(NewYearHS);
+        System.out.println("\nTreeSet с компаратором в обратном порядке (внутри копия множества HashSet): " + NewYearTS);
 
     }
 }
